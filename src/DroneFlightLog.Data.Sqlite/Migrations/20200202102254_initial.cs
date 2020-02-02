62eca61f9c0f6dd2eb177b124d5296bfd6ed94c0
David Walker <david.walker5@icloud.com>
2020-02-02 10:40:48 +0000

    Added EF Core Migrations

---
+++ src/DroneFlightLog.Data.Sqlite/Migrations/20200202102254_initial.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DroneFlightLog.Data.Sqlite.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Number = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    Town = table.Column<string>(nullable: true),
+                    County = table.Column<string>(nullable: true),
+                    Postcode = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlightProperties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    DataType = table.Column<int>(nullable: false),
+                    IsSingleInstance = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightProperties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manufacturers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Operators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AddressId = table.Column<int>(nullable: false),
+                    FirstNames = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    OperatorNumber = table.Column<string>(nullable: true),
+                    FlyerNumber = table.Column<string>(nullable: true),
+                    DoB = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Operators", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Operators_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Models",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ManufacturerId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Models", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Models_Manufacturers_ManufacturerId",
+                        column: x => x.ManufacturerId,
+                        principalTable: "Manufacturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ModelId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    SerialNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Drones_Models_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Models",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DroneId = table.Column<int>(nullable: false),
+                    LocationId = table.Column<int>(nullable: false),
+                    OperatorId = table.Column<int>(nullable: false),
+                    Start = table.Column<DateTime>(nullable: false),
+                    End = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flights_Drones_DroneId",
+                        column: x => x.DroneId,
+                        principalTable: "Drones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flights_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flights_Operators_OperatorId",
+                        column: x => x.OperatorId,
+                        principalTable: "Operators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlightPropertyValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PropertyId = table.Column<int>(nullable: false),
+                    FlightId = table.Column<int>(nullable: false),
+                    StringValue = table.Column<string>(nullable: true),
+                    DateValue = table.Column<DateTime>(nullable: true),
+                    NumberValue = table.Column<decimal>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightPropertyValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FlightPropertyValues_Flights_FlightId",
+                        column: x => x.FlightId,
+                        principalTable: "Flights",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FlightPropertyValues_FlightProperties_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "FlightProperties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drones_ModelId",
+                table: "Drones",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlightPropertyValues_FlightId",
+                table: "FlightPropertyValues",
+                column: "FlightId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlightPropertyValues_PropertyId",
+                table: "FlightPropertyValues",
+                column: "PropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_DroneId",
+                table: "Flights",
+                column: "DroneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_LocationId",
+                table: "Flights",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_OperatorId",
+                table: "Flights",
+                column: "OperatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Models_ManufacturerId",
+                table: "Models",
+                column: "ManufacturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Operators_AddressId",
+                table: "Operators",
+                column: "AddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlightPropertyValues");
+
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "FlightProperties");
+
+            migrationBuilder.DropTable(
+                name: "Drones");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Operators");
+
+            migrationBuilder.DropTable(
+                name: "Models");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Manufacturers");
+        }
+    }
+}
