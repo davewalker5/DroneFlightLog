b7639a3019aa979091602efd96695fdde4df50b1
David Walker <dwalker@cellmark.co.uk>
2020-05-14 18:11:05 +0100

    Added database management app

---
+++ src/DroneFlightLog.Manager/Logic/CommandParser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using DroneFlightLog.Manager.Entities;
+
+namespace DroneFlightLog.Manager.Logic
+{
+    public class CommandParser
+    {
+        // The index into this array is one of the values from the OperationType
+        // enumeration, mapping the operation to the required argument count
+        private readonly int[] _requiredArgumentCount = { 3, 3, 2, 2, 2, 1 };
+
+        /// <summary>
+        /// Parse the command line, extracting the operation to be performed
+        /// and its parameters 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public Operation ParseCommandLine(string[] args)
+        {
+            Operation op = new Operation();
+
+            if (args.Length > 0)
+            {
+                // Attempt to parse out the operation type from the first argument
+                if (Enum.TryParse<OperationType>(args[0], out OperationType type))
+                {
+                    // Check there are sufficient arguments for this operation
+                    op.Type = type;
+                    int requiredArgumentCount = _requiredArgumentCount[(int)type];
+
+                    // All is OK at this point if the argument count is correct
+                    op.Valid = (args.Length == requiredArgumentCount);
+                    if (op.Valid)
+                    {
+                        // Extract the arguments
+                        AssignOperationParameters(op, args);
+                    }
+                }
+            }
+
+            return op;
+        }
+
+        /// <summary>
+        /// For those operations that require it, assign the operation parameters
+        /// from the command line, based on the operation type
+        /// </summary>
+        /// <param name="op"></param>
+        /// <param name="args"></param>
+        private void AssignOperationParameters(Operation op, string[] args)
+        {
+            switch (op.Type)
+            {
+                case OperationType.add:
+                    op.UserName = args[1];
+                    op.Password = args[2];
+                    break;
+                case OperationType.delete:
+                    op.UserName = args[1];
+                    break;
+                case OperationType.setpassword:
+                    op.UserName = args[1];
+                    op.Password = args[2];
+                    break;
+                case OperationType.import:
+                    op.FileName = args[1];
+                    break;
+                case OperationType.export:
+                    op.FileName = args[1];
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ src/DroneFlightLog.Manager/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using DroneFlightLog.Data.Factory;
+using DroneFlightLog.Data.Sqlite;
+using DroneFlightLog.Manager.Entities;
+using DroneFlightLog.Manager.Logic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DroneFlightLog.Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string version = typeof(Program).Assembly.GetName().Version.ToString();
+            Console.WriteLine($"Drone Flight Log Database Management {version}");
+
+            Operation op = new CommandParser().ParseCommandLine(args);
+            if (op.Valid)
+            {
+                DroneFlightLogDbContext context = new DroneFlightLogDbContextFactory().CreateDbContext(null);
+                DroneFlightLogFactory<DroneFlightLogDbContext> factory = new DroneFlightLogFactory<DroneFlightLogDbContext>(context);
+
+                try
+                {
+                    switch (op.Type)
+                    {
+                        case OperationType.add:
+                            factory.Users.AddUser(op.UserName, op.Password);
+                            Console.WriteLine($"Added user {op.UserName}");
+                            break;
+                        case OperationType.setpassword:
+                            factory.Users.SetPassword(op.UserName, op.Password);
+                            Console.WriteLine($"Set password for user {op.UserName}");
+                            break;
+                        case OperationType.delete:
+                            factory.Users.DeleteUser(op.UserName);
+                            Console.WriteLine($"Deleted user {op.UserName}");
+                            break;
+                        case OperationType.update:
+                            context.Database.Migrate();
+                            Console.WriteLine($"Applied the latest database migrations");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error : {ex.Message}");
+                }
+            }
+            else
+            {
+                string executable = AppDomain.CurrentDomain.FriendlyName;
+                Console.WriteLine("Usage:");
+                Console.WriteLine($"[1] {executable} add username password");
+                Console.WriteLine($"[2] {executable} setpassword username password");
+                Console.WriteLine($"[3] {executable} delete username");
+                Console.WriteLine($"[4] {executable} update");
+            }
+        }
+    }
+}
